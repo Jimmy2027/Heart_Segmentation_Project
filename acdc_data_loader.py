1263414fad69efcde002c5c6dc83e1a6436ce4af
klugh <klugh@student.ethz.ch>
2019-04-11 11:25:18 +0200

    implemented cropping function,using center of mass of mycoardium label

--- acdc_data_loader.py
+++ acdc_data_loader.py
@@ -43,7 +43,6 @@ def remove_other_segmentations(labels):
         myocar_data.append(img_data)
     return myocar_data
 
-#TODO load image data as well as data paths
 
 def load_images(data_dir, raw_image_path01, raw_image_path12, label_path01, label_path12):
     """
@@ -53,11 +52,11 @@ def load_images(data_dir, raw_image_path01, raw_image_path12, label_path01, labe
     """
     im_data = []
     labels = []
-    for o in os.listdir(data_dir): #o: patientxxx
+    for o in os.listdir(data_dir): # o: patientxxx
         # print('o: ', o)
         if not o.startswith('.') and os.path.isdir(os.path.join(data_dir, o)):
 
-            for x in os.listdir(os.path.join(data_dir, o)): #x: patientXXX_frameXX.nii.gz
+            for x in os.listdir(os.path.join(data_dir, o)): # x: patientXXX_frameXX.nii.gz
 
                 if not x.startswith('.'):
                     # print('x: ' , data_dir + '/' + x)
@@ -118,141 +117,63 @@ def display(img_data, block = True):
 
 
 def find_center_of_mass(data):
-    return nd.measurements.center_of_mass(data)
-
-
-
-
-def find_middle(img_data):  #TODO here I am taking the middle of the image, what I want is to have the middle of the segmentation though
     """
 
-    :param img_data: single image data
-    :return: center coordinate of input image
+    :param data: array of images with shape (n_i, n_j, n_k)
+    :return: list of arrays (of length n_k)  with the center of mass for each of these images (n_i, n_j) and list of empty labels
+    """
+    coms = []
+    print(np.shape(data))
+    empty_labels = []
+    for s in range(0, len(data)):
+        com = []
+        for i in range(0, data[s].shape[2]):
+            temp = np.array(sum(data[s][..., [i]]))
+            bool_temp = temp != 0
+
+            if sum(bool_temp) != 0:         #verify that label is non empty
+                com.append(nd.measurements.center_of_mass(data[s][..., [i]][..., 0]))
+
+            else: empty_labels.append((s, i))
+
+        coms.append(com)
+
+    print(np.shape(coms))
+    return coms, empty_labels
+
+
+def crop_images(cropper_size, center_of_masses, data):
     """
 
-    n_i, n_j, n_k = img_data.shape
-    center_i = (n_i - 1) // 2  # // for integer division
-    center_j = (n_j - 1) // 2
-    center_k = (n_k - 1) // 2
-
-    img_center = [center_i, center_j, center_k]
-    # print('Image center: ', center_i, center_j, center_k)
-    center_vox_value = img_data[center_i, center_j, center_k]
-    # print('Image center value: ', center_vox_value)
-
-    return img_center
-
-
-def plot_img_with_label(labels, images):
+    :param cropper_size:
+    :param center_of_masses:
+    :param data:
+    :return: returns np.array of cropped images
     """
-    Plots 50 images with labels
-    :param labels: list with label paths
-    :param images: list with images paths
-    """
-    for i in range(1, 50):
-        plotting.plot_roi(labels[i], bg_img=images[i], cmap='Paired')
-        # plotting.plot_img(i)
-        plotting.show()
+    cropped_data = []
+    for s in range(0, len(data)-1):
+        temp = np.empty((data[s].shape[2], 2*cropper_size, 2*cropper_size))
+        for i in range(0, data[s].shape[2]-1):
+
+            center_i = int(center_of_masses[s][i][0])
+            center_j = int(center_of_masses[s][i][1])
+
+            temp[i] = data[s][..., i][center_i - cropper_size: center_i + cropper_size, center_j - cropper_size: center_j + cropper_size]
+
+        cropped_data.append(temp)
+    return cropped_data
 
 
-def crop_images(cropper_size, average_i, average_j, average_k, img_data, myocar_labels):        #TODO ATTENTION: data shape not uniform... how can I uniformise the shapes?
-    """
-    Data shape: (216, 256, 10)
-    :param average_i:
-    :param average_j:
-    :param average_k:
-    :param img_data: data_array of to be cropped image
-    :param myocar_labels: data_array of to be copped labels
-    :return: data arrays of cropped images and labels
-    """
-    print('len img data:', len(img_data))
-    print('len myo labels', len(myocar_labels))
-    min_layer = 8
-    data_shape = img_data[0].shape
-    cropped_img_data = np.empty((len(img_data), 100, 100, min_layer))
-    cropped_myocar_labels = np.empty((len(myocar_labels), 100, 100, min_layer))
-    for t, i in enumerate(img_data):
-        print('t:',t)
-        print('i shape:', i.shape)
-        ishape = i.shape
-        if ishape[2] > min_layer:     #if image has more layers than usual, remove them      TODO is this okay?
-            i = i[:,:,0:min_layer]
-            print('new i shape:', i.shape)
-
-        if ishape[0] != data_shape[0] or ishape[1] != data_shape[1]:
-            i = np.empty((100, 100, min_layer))
-            print('new i shape:', i.shape)
-        cropped_img_data[t] = i[average_i - cropper_size: average_i + cropper_size, average_j - cropper_size:average_j + cropper_size, :]
-
-    for t, i in enumerate(myocar_labels):
-        ishape = i.shape
-        if ishape[2] > min_layer:     #if image has more layers than usual, remove them      TODO is this okay?
-            i = i[:,:,0:min_layer]
-            print('new i shape:', i.shape)
-
-        if ishape[0] != data_shape[0] or ishape[1] != data_shape[1]:
-            i = np.empty((100, 100, min_layer))
-            print('new i shape:', i.shape)
-
-        cropped_myocar_labels[t] = i[average_i - cropper_size: average_i + cropper_size, average_j - cropper_size: average_j + cropper_size, :]
-
-    return cropped_img_data, cropped_myocar_labels
 
 
-def crop_images2(cropper_size, data_shape, data_length, average_i, average_j, average_k, img_data, myocar_labels):        #TODO ATTENTION: data shape not uniform... how can I uniformise the shapes?
-    """
-    This function only crops images with the data shape (216, 256, 10)
-    :param average_i:
-    :param average_j:
-    :param average_k:
-    :param img_data: data_array of to be cropped image
-    :param myocar_labels: data_array of to be copped labels
-    :return: data arrays of cropped images and labels
-    """
-    print('len img data:', len(img_data))
-    print('len myo labels', len(myocar_labels))
-    data_shape = img_data[0].shape
-    cropped_img_data = np.empty((data_length, 100, 100, 10))
-    cropped_myocar_labels = np.empty((len(myocar_labels), 100, 100, 10))
-    t_img = 0
-    t_label = 0
-    for i in img_data:
-        if i.shape == data_shape:
+def remove_empty_label_data(data, empty_labels):
+    for i in empty_labels:
 
-            cropped_img_data[t_img] = i[average_i - cropper_size: average_i + cropper_size, average_j - cropper_size:average_j + cropper_size, :]
-            t_img= t_img+1
+        data[i[0]] = np.delete(data[i[0]], i[1], 2)
+    return data
 
-    for i in myocar_labels:
-        if i.shape == data_shape:
 
-            cropped_myocar_labels[t_label] = i[average_i - cropper_size: average_i + cropper_size, average_j - cropper_size: average_j + cropper_size, :]
-            t_label = t_label + 1
 
-    return cropped_img_data, cropped_myocar_labels
-
-def find_most_common_shape2(data):
-    """
-
-    :param data: data Array
-    :return: list of most common element shape in array and number of occurences
-    """
-    temp = []
-    for t, i in enumerate(data):
-        temp.append(i.shape)
-    b = Counter(temp)
-    return b.most_common(1)
-
-def find_most_common_shape(data):
-    """
-
-    :param data: data Array
-    :return: list of most common element shape in array and number of occurences
-    """
-    temp = np.empty((len(data)))
-    for t, i in enumerate(data):
-        temp[t] = i.shape
-    b = Counter(temp)
-    return b.most_common(1)
 
 
 # data_path = 'ACDC_dataset/training/patient001/patient001_frame01.nii.gz'
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 """
 TODO:
+    - need to flatten the slices for every person?
     - find segmentation centers
     - need to
     - normalize the data
 
 
 """
-
-
 import acdc_data_loader as acdc
-import nibabel as nib
+from matplotlib import pyplot as plt
 import numpy as np
-from nilearn import plotting
 
-
-"""
-Data shape: (216, 256, 10)
-"""
 
 data_dir = 'ACDC_dataset/training'
 raw_image_path01 = '_frame01.nii.gz'
@@ -26,53 +20,41 @@ label_path12 = '_frame12_gt.nii.gz'
 
 
 images_paths, labels_paths = acdc.load_images(data_dir, raw_image_path01, raw_image_path12, label_path01, label_path12)
-images_paths.sort()   #each label should be on the same index than its corresponding image
+images_paths.sort()   # each label should be on the same index than its corresponding image
 labels_paths.sort()
 
-myocar_labels = acdc.remove_other_segmentations(labels_paths)  #array with all the myocar labels
-
-img_data = acdc.load_data(images_paths
-                          )
-for i in img_data:
-    print('data shape: ', i.shape)
-
-# for i in range(0,10):
-#     acdc.display(myocar_labels[i])
-
-# normalized_data = acdc.data_normalization(img_data)
-label_centers = []
-centers_i = []
-centers_j = []
-centers_k = []
-for i in myocar_labels:
-    print('label shape: ', i.shape)
-    label_centers.append(acdc.find_middle(i))
-    centers_i.append(acdc.find_middle(i)[0])
-    centers_j.append(acdc.find_middle(i)[1])
-    centers_k.append(acdc.find_middle(i)[2])
-
-average_i = sum(centers_i)//len(centers_i)
-average_j = sum(centers_j)//len(centers_j)
-average_k = sum(centers_k)//len(centers_k)
-average_center = [average_i, average_j, average_k]
-print('average center:', average_center)
-cropper_size = 50
-print(myocar_labels[0][average_i - cropper_size: average_i + cropper_size, average_j - cropper_size:average_j + cropper_size, :].shape)
+myocar_labels = acdc.remove_other_segmentations(labels_paths)  # array with all the myocar labels
+img_data = acdc.load_data(images_paths)  # array with all the heart images
+shape = img_data[0].shape
 
 
-most_common_dataShape = acdc.find_most_common_shape2(img_data)[0][0]
-mc_dataShape_occurences = acdc.find_most_common_shape2(img_data)[0][1]          #only 12 images have this shape
+center_of_masses, empty_labels = acdc.find_center_of_mass(myocar_labels)
+empty_labels.sort(reverse=True)  # need to do this for remove_empty_labels
 
 
-print(most_common_dataShape)
-print(mc_dataShape_occurences)
+
+myocar_labels = acdc.remove_empty_label_data(myocar_labels, empty_labels)       # need to remove empty labels from labels and image_data
+img_data = acdc.remove_empty_label_data(img_data, empty_labels)
 
 
 
 
-cropped_img_data, cropped_myocar_labels = acdc.crop_images2(cropper_size, most_common_dataShape, mc_dataShape_occurences, average_i, average_j, average_k, img_data, myocar_labels)
+cropper_size = 32
+cropped_img_data = acdc.crop_images(cropper_size, center_of_masses, img_data)
+cropped_myocar_labels = acdc.crop_images(cropper_size, center_of_masses, myocar_labels)
 
-for i in range(0, mc_dataShape_occurences):
+
+# for s in range(0, 10):
+#     for i in range(0, cropped_img_data[s].shape[2]-1):
+#         plt.imshow(cropped_myocar_labels[s][i])
+#         plt.show()
+#         plt.imshow(cropped_img_data[s][i])
+#         plt.show()
+
+
+
+
+for i in range(0, 10):
     acdc.display(cropped_myocar_labels[i])
     acdc.display(cropped_img_data[i])
 
