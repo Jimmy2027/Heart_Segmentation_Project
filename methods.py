1055e7405b443e006bfe04fc648c599ff04aab78
klugh <klugh@student.ethz.ch>
2019-05-13 18:42:57 +0200

    added epochs as parameter for the unet created for loop for different amount of data

--- methods.py
+++ methods.py
@@ -246,42 +246,6 @@ def save_datavisualisation2(img_data, myocar_labels, save_folder, index_first = 
         imageio.imwrite('visualisation/' + save_folder + '%d.png' % (counter,), image)
         counter = counter + 1
 
-def save_datavisualisation2(img_data, myocar_labels, save_folder, index_first = False, normalized = False):
-    if index_first == True:
-        for i in range(0, len(img_data)):
-            img_data[i] = np.moveaxis(img_data[i], 0, -1)
-            myocar_labels[i] = np.moveaxis(myocar_labels[i], 0, -1)
-
-    counter = 0
-    for i, j in zip(img_data[:], myocar_labels[:]):
-        print(counter)
-        print(i.shape)
-        i_patch = i[:, :, 0]
-        if normalized == True:
-            i_patch = i_patch*255
-        # np.squeeze(i_patch)
-
-        j_patch = j[:, :, 0]
-        # np.squeeze(j_patch)
-        j_patch = j_patch * 255
-        for slice in range(1, i.shape[2]):
-            temp = i[:, :, slice]
-            # np.squeeze(temp)
-            if normalized == True:
-                temp = temp * 255
-            i_patch = np.hstack((i_patch, temp))
-
-
-            temp = j[:, :, slice]
-            # np.squeeze(temp)
-            temp = temp * 255
-            j_patch = np.hstack((j_patch, temp))
-
-        image = np.vstack((i_patch, j_patch))
-
-        print(image.shape)
-        imageio.imwrite('visualisation/' + save_folder + '%d.png' % (counter,), image)
-        counter = counter + 1
 
 
 def save_datavisualisation3(img_data, myocar_labels, predicted_labels, save_folder, index_first = False, normalized = False):
--- network_trainer.py
+++ network_trainer.py
@@ -16,11 +16,14 @@ from sklearn.metrics import roc_curve, auc
 whichdataset = 'York'
 # whichmodel = 'param_unet'
 whichmodel = 'twolayernetwork'
+# whichmodel = 'segnetwork'
 
-number_of_patients = 10
+
+# number_of_patients = 10
 filters = 64
 # max 5 layers with 96x96
 layers = 2
+epochs = 1
 
 
 
@@ -56,77 +59,81 @@ unet_labels = []
 #TODO variable amount of slices per person? (in percentages)
 total_number_of_patients = len(input)
 
-random_patient_num = [random.sample(range(total_number_of_patients-1), number_of_patients)]
 
-for i in random_patient_num[0]:
-    unet_input.append(input[i])
-    unet_labels.append(labels[i])
-
-x_train, x_test , y_train, y_test = model_selection.train_test_split(unet_input, unet_labels, test_size=0.3)
-
-
-x_train = np.concatenate(x_train, axis = 0)
-y_train = np.concatenate(y_train, axis = 0)
-
-x_train = np.expand_dims(x_train, -1)  #-1 for the last element
-y_train = np.expand_dims(y_train, -1)
+arr_number_of_patients = [total_number_of_patients-1, int(total_number_of_patients*0.25), int(total_number_of_patients*0.5), int(total_number_of_patients*0.75)]
 
 
 
-resolution = x_train.shape[1]
+for number_of_patients in arr_number_of_patients:
 
-print(np.shape(x_train), np.shape(y_train))
+    random_patient_num = [random.sample(range(total_number_of_patients-1), number_of_patients)]
 
-# conv 2D default parameter: channels last: (batch, rows, cols, channels)
+    for i in random_patient_num[0]:
+        unet_input.append(input[i])
+        unet_labels.append(labels[i])
 
-validation_split_val = 0.25
-batch_size = 32
-epochs = 1
-input_size = (resolution, resolution, 1)
-kernel_size = 3
-Dropout_rate = 0.5
+    x_train, x_test , y_train, y_test = model_selection.train_test_split(unet_input, unet_labels, test_size=0.3)
 
 
+    x_train = np.concatenate(x_train, axis = 0)
+    y_train = np.concatenate(y_train, axis = 0)
+
+    x_train = np.expand_dims(x_train, -1)  #-1 for the last element
+    y_train = np.expand_dims(y_train, -1)
 
 
 
-if whichmodel == 'param_unet':
-    model = unet.param_unet(input_size, filters, layers)
+    resolution = x_train.shape[1]
 
+    print(np.shape(x_train), np.shape(y_train))
 
-if whichmodel == 'twolayernetwork':
-    model = unet.twolayernetwork(input_size, kernel_size, Dropout_rate)
+    # conv 2D default parameter: channels last: (batch, rows, cols, channels)
+
+    validation_split_val = 0.25
+    batch_size = 32
+    input_size = (resolution, resolution, 1)
+    kernel_size = 3
+    Dropout_rate = 0.5
 
 
 
 
 
-if not os.path.exists(path):
-    os.makedirs(path)
-if not os.path.exists(path + '/' + whichmodel):
-    os.makedirs(path + '/' + whichmodel)
-if not os.path.exists(path + '/'+whichmodel+'/'+str(number_of_patients)+'patients'):
-    os.makedirs(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients')
-if not os.path.exists(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients/' + str(layers)+'layers'):
-    os.makedirs(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients/' + str(layers)+'layers')
-save_dir = path + '/' + whichmodel +'/' + str(number_of_patients)+'patients/'+ str(layers)+'layers'
+    if whichmodel == 'param_unet':
+        model = unet.param_unet(input_size, filters, layers)
 
 
-if whichmodel == 'param_unet' or whichmodel == 'unet':
+    if whichmodel == 'twolayernetwork':
+        model = unet.twolayernetwork(input_size, kernel_size, Dropout_rate)
 
-    model_checkpoint = ModelCheckpoint(save_dir+'/unet.{epoch:02d}.hdf5', monitor='loss', verbose=1, save_best_only=True)
-    history = model.fit(x_train, y_train, steps_per_epoch=1, epochs=8, callbacks=[model_checkpoint])
-else:
-    model.summary()
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    history = model.fit(x_train, y_train, validation_split=validation_split_val, epochs=epochs, verbose=1)
+    if whichmodel == 'segnetwork':
+        model = unet.segnetwork(input_size, kernel_size, Dropout_rate)
 
 
 
 
 
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if not os.path.exists(path + '/' + whichmodel):
+        os.makedirs(path + '/' + whichmodel)
+    if not os.path.exists(path + '/'+whichmodel+'/'+str(number_of_patients)+'patients'):
+        os.makedirs(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients')
+    if not os.path.exists(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients/' + str(layers)+'layers'):
+        os.makedirs(path + '/'+whichmodel+'/'+ str(number_of_patients)+'patients/' + str(layers)+'layers')
+    save_dir = path + '/' + whichmodel +'/' + str(number_of_patients)+'patients/'+ str(layers)+'layers'
+
+
+    if whichmodel == 'param_unet' or whichmodel == 'unet':
+
+        model_checkpoint = ModelCheckpoint(save_dir+'/unet.{epoch:02d}.hdf5', monitor='loss', verbose=1, save_best_only=True)
+        history = model.fit(x_train, y_train, steps_per_epoch=1, epochs=epochs, callbacks=[model_checkpoint])
+    else:
+        model.summary()
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        history = model.fit(x_train, y_train, validation_split=validation_split_val, epochs=epochs, verbose=1)
 
 
 
@@ -135,82 +142,84 @@ else:
 
 
 
-print(history.history.keys())
 
 
-#
-# # Plot training & validation accuracy values
-# plt.plot(history.history['acc'])
-# plt.plot(history.history['val_acc'])
-# plt.title('Model accuracy')
-# plt.ylabel('Accuracy')
-# plt.xlabel('Epoch')
-# plt.legend(['Train', 'Test'], loc='upper left')
-# plt.show()
-# plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_accuracy_values.png'))
-#
-# # Plot training & validation loss values
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('Model loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epoch')
-# plt.legend(['Train', 'Test'], loc='upper left')
-# plt.show()
-# plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_loss_values.png'))
-#
-# model.save(os.path.join(save_dir, str(epochs) +'epochs_test.h5'))
-
-y_pred = []
-for i in x_test:
-    i = np.expand_dims(i, -1)
-    y_pred.append(model.predict(i, verbose = 1))
-
-np.save(os.path.join(save_dir, str(epochs) + 'epochs_y_pred'), y_pred)
+    print(history.history.keys())
 
 
+    #
+    # # Plot training & validation accuracy values
+    # plt.plot(history.history['acc'])
+    # plt.plot(history.history['val_acc'])
+    # plt.title('Model accuracy')
+    # plt.ylabel('Accuracy')
+    # plt.xlabel('Epoch')
+    # plt.legend(['Train', 'Test'], loc='upper left')
+    # plt.show()
+    # plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_accuracy_values.png'))
+    #
+    # # Plot training & validation loss values
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    # plt.title('Model loss')
+    # plt.ylabel('Loss')
+    # plt.xlabel('Epoch')
+    # plt.legend(['Train', 'Test'], loc='upper left')
+    # plt.show()
+    # plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_loss_values.png'))
+    #
+    # model.save(os.path.join(save_dir, str(epochs) +'epochs_test.h5'))
 
-thresholds = [0.1, 0.25, 0.5, 0.75]
+    y_pred = []
+    for i in x_test:
+        i = np.expand_dims(i, -1)
+        y_pred.append(model.predict(i, verbose = 1))
 
-for threshold in thresholds:
-
-    upper, lower = 1, 0
-
-    results = {
-        "median_ROC_AUC": "median_ROC_AUC",
-        "dice": "dice",
-        "roc_auc": "roc_auc",
-        "median_dice_score": "median_dice_score"
-    }
-    dice = []
-    roc_auc = []
-    output = []
-
-    for i in range(len(y_test)):
-        output.append(np.squeeze(y_pred[i]))
-        for s in range(y_test[i].shape[0]):
-            output[i][s] = np.where(output[i][s] > threshold, upper, lower)
-
-            dice.append(su.dice(output[i][s], y_test[i][s]))
-            y_true = y_test[i][s].reshape(-1)
-            y_pred_temp = y_pred[i][s].reshape(-1)
-            fpr, tpr, thresholds = roc_curve(y_true, y_pred_temp)
-            roc_auc.append(auc(fpr, tpr))
+    np.save(os.path.join(save_dir, str(epochs) + 'epochs_y_pred'), y_pred)
 
 
-    median_ROC_AUC = np.median(roc_auc)
-    median_dice_score = np.median(dice)
 
-    results["median_dice_score"] = median_dice_score
-    results["median_ROC_AUC"] = median_ROC_AUC
-    results["dice"] = dice
-    results["roc_auc"] = roc_auc
-    torch.save(results, os.path.join(save_dir, str(epochs) + 'epochs_evaluation_results'))
+    thresholds = [0.1, 0.25, 0.5, 0.75]
 
-    print('DICE SCORE: ' + str(median_dice_score))
-    print('ROC AUC:', str(median_ROC_AUC))
+    for threshold in thresholds:
 
-    methods.save_datavisualisation3(x_test, y_test, output, os.path.join(save_dir,str(threshold) + 'thr_'), True, True)
+        upper, lower = 1, 0
+
+        results = {
+            "median_ROC_AUC": "median_ROC_AUC",
+            "dice": "dice",
+            "roc_auc": "roc_auc",
+            "median_dice_score": "median_dice_score"
+        }
+        dice = []
+        roc_auc = []
+        output = []
+
+        for i in range(len(y_test)):
+            output.append(np.squeeze(y_pred[i]))
+            for s in range(y_test[i].shape[0]):
+                output[i][s] = np.where(output[i][s] > threshold, upper, lower)
+
+                dice.append(su.dice(output[i][s], y_test[i][s]))
+                y_true = y_test[i][s].reshape(-1)
+                y_pred_temp = y_pred[i][s].reshape(-1)
+                fpr, tpr, thresholds = roc_curve(y_true, y_pred_temp)
+                roc_auc.append(auc(fpr, tpr))
+
+
+        median_ROC_AUC = np.median(roc_auc)
+        median_dice_score = np.median(dice)
+
+        results["median_dice_score"] = median_dice_score
+        results["median_ROC_AUC"] = median_ROC_AUC
+        results["dice"] = dice
+        results["roc_auc"] = roc_auc
+        torch.save(results, os.path.join(save_dir, str(epochs) + 'epochs_evaluation_results'))
+
+        print('DICE SCORE: ' + str(median_dice_score))
+        print('ROC AUC:', str(median_ROC_AUC))
+
+        methods.save_datavisualisation3(x_test, y_test, output, os.path.join(save_dir,str(threshold) + 'thr_'), True, True)
 
 
 
